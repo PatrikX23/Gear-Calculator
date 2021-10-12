ebe589e8fde0e160bfc4c0404c7da6046b862c79
Patrik <patrikben100hun@gmail.com>
2021-10-12 18:54:27 +0200

    Rework part1

--- calc.py
+++ calc.py
@@ -1,7 +1,3 @@
-######################
-# Created by PatrikX #
-# Tested by Csanaboy #
-######################
 import os, time, sys, math
 clear = lambda: os.system('cls')
 clear()
@@ -9,22 +5,13 @@ clear()
 kompActive = False
 defaultActive = False
 
-z1active = False
-z2active = False
-iactive = False
-n1active = False
-n2active = False
-
 datasdone = False
+menuActive = False
+typeInput = False
+navInput = False
 
-z1 = -1
-z2 = -1
-m = -1
-i = -1
-n1 = -1
-n2 = -1
-cstar = 0.25
-
+loading = 0
+loadtime = 5
 
 class bcolors:
     BLUE = "\033[94m"
@@ -32,204 +19,170 @@ class bcolors:
     YELLOW = "\033[1;33;40m"
     GREEN = "\033[1;32;40m"
     RED = "\033[1;31;40m"
+    BG = "\033[0;37;41m"
+    BG2 = "\033[0;37;44m"
 
-print(bcolors.GREEN +"\n######################\n# Created by PatrikX #\n# Tested by Csanaboy #\n######################"+ bcolors.WHITE)
-print("\n")
-print(bcolors.YELLOW +"Választási lehetőségek:\n\n" + bcolors.BLUE + "Elemi (z1 > 16) >> 1"+ bcolors.RED +" (Nem elérhető)\n\n" + bcolors.BLUE + "Kompenzált (z1 < 17) >> 2 "+ bcolors.GREEN +"(Elérhető)" + bcolors.WHITE)
-typeInput = input(bcolors.BLUE + "\nVálasztott típus: " + bcolors.WHITE)
+def loadElemi():
+    global loading
 
-if typeInput == str("1"):
-    clear()
-    print(bcolors.YELLOW + "\nElemi fogaskerék számítás" + bcolors.WHITE)
-    #defaultActive = True
-    kompActive = True
-    inputZ1 = input("Z1: ")
-elif typeInput == str("2"):
-    clear()
-    print(bcolors.YELLOW + "\nKompenzált fogaskerék számítás" + bcolors.WHITE)
-    kompActive = True
-    inputZ1 = input("Z1: ")
+    while (loading < loadtime):
+        loading += 0.25
+        sys.stdout.write('\rSzámítás |')
+        time.sleep(0.1)
+        loading += 0.25
+        sys.stdout.write('\rSzámítás /')
+        time.sleep(0.1)
+        loading += 0.25
+        sys.stdout.write('\rSzámítás -')
+        time.sleep(0.1)
+        loading += 0.25
+        sys.stdout.write('\rSzámítás \\')
+        time.sleep(0.1)
+    else:
+        clear()
+        time.sleep(0.5)
+        calcElemi()
 
-# elemi
-if defaultActive:
-    if int(inputZ1) >= 17:
-        z1 = inputZ1
-        z1active = True
-        inputZ2 = input("Z2: ")
-    elif int(inputZ1) == -1:
-        z1 = -1
-        z1active = False
-        inputZ2 = input("Z2: ")
-    elif int(inputZ1) < 17:
-        print(bcolors.RED + "Hiba Z1 kisebb mint 17" + bcolors.WHITE)
-        sys.exit()
-    
-    if int(inputZ2) >= 17:
-        z2 = inputZ2
-        z2active = True
-        inputM = input("m: ")
-    elif int(inputZ2) == -1:
-        z2 = -1
-        z2active = False
-        inputM = input("m: ")
-    elif int(inputZ2) < 17:
-        print(bcolors.RED + "Hiba Z2 kisebb mint 17" + bcolors.WHITE)
-        sys.exit()
-    
-    if int(float(inputM)) > 0.0:
-        m = float(inputM)
-        inputI = input("i: ")
-    elif inputM == 0:
-        m = -1
-        inputI = input("i: ")
-    elif int(float(inputM)) < 1.0:
-        print(bcolors.RED + "Hiba: m nem lehet 0" + bcolors.WHITE)
-        sys.exit()
-    
-    if int(float(inputI)) > 0.0:
-        i = float(inputI)
-        iactive = True
-        inputN1 = input("n1: ")
-    elif int(inputI) == 0:
-        i = -1
-        iactive = False
-        inputN1 = input("n1: ")
-    
-    if int(inputN1) > 0:
-        n1 = inputN1
-        n1active = True
-        inputN2 = input("n2: ")
-    elif int(inputN1) == 0:
-        n1 = -1
-        n1active = False
-        inputN2 = input("n2: ")
-    
-    if int(inputN2) > 0:
-        n2 = inputN2
-        n2active = True
-        inputCstar = input("c*: ")
-        #datasdone = True
-    elif int(inputN2) == 0:
-        n2 = -1
-        inputCstar = input("c*: ")
-        n2active = False
-        #datasdone = True
-    
-    if int(float(inputCstar)) >= 0:
-        cstar = float(inputCstar)
-        datasdone = True
+def menu():
+    global menuActive
+    global typeInput
+    global datasdone
+    global navInput
+   
+    datasdone = False
+    typeInput = False
+    navInput = False
+    menuActive = False
+    print(bcolors.GREEN +"\n######################\n# Created by PatrikX #\n# Tested by Csanaboy #\n######################"+ bcolors.WHITE)
+    print(bcolors.YELLOW +"\nVálasztási lehetőségek:\n\n" + bcolors.BLUE + "Elemi (z1 > 16) >> 1"+ bcolors.GREEN +" (Elérhető)\n\n" + bcolors.BLUE + "Kompenzált (z1 < 17) >> 2 "+ bcolors.RED +"(Nem Elérhető)" + bcolors.WHITE)
+    time.sleep(0.5)
+    menuActive = True
+    typeInput = input(bcolors.BLUE + "\nVálasztott típus: " + bcolors.WHITE)
 
-    if datasdone:
-        if z1active and z2active == False and iactive:
-            z2 = int(z1) * float(i)
-            print("\nEredmények:\nz2: "+ str(z2))
-        elif z1active == False and z2active and iactive:
-            z1 = int(z2) / float(i)
-            print("\nEredmények:\nz1: "+ str(z1))
-        elif z1active == False and z2active and iactive == False and n1active and n2active:
-            i = int(n1) / float(n2)
-            z1 = int(z2) / float(i)
-            print("\nEredmények:\nz1: "+ str(z1) + "\ni: "+ str(i))
-        elif z1active and z2active == False and iactive == False and n1active and n2active:
-            i = int(n1) / float(n2)
-            c = float(cstar) * float(m)
-            z2 = int(z1) * float(i)
-            time.sleep(1)
-            d1 = int(m) * int(z1)
-            d2 = int(m) * int(z2)
-            da1 = (int(d1) + (float(m)*2))
-            da2 = (int(d2) + (float(m)*2))
-            db1 = (int(d1)*math.cos(20))
-            db2 = (int(d2)*math.cos(20))
-            df1 = (int(d1)-(float(m)*2)-(float(c)*2))
-            df2 = (int(d2)-(float(m)*2)-(float(c)*2))
-            time.sleep(2)
-            print("\nEredmények:\nz2: "+ str(z2) + "\ni: "+ str(i) + "\nd1: "+ str(d1) + "\nd2: "+ str(d2)+ "\nda1: "+ str(da1) + "\nda2: "+ str(da2)+ "\ndb1: "+ str(db1) + "\ndb2: "+ str(db2)+ "\ndf1: "+ str(df1) + "\ndf2: "+ str(df2))
-           # print("\nd1:"+ str(d1)) 
-           ## print("d2: "+ str(d2))  
+time.sleep(0.5)
+menu()
+
+def calcElemi():
+    global datasdone
+    global navInput
+
+    kszog = math.radians(20)
+    c = float(m) * float(cstar)
+    d1 = float(m) * float(z1)
+    d2 = float(m) * float(z2)
+    da1 = (float(d1) + (2*float(m)))
+    da2 = (float(d2) + (2*float(m)))
+    db1 = (float(d1) + (math.cos(kszog)))
+    db2 = (float(d2) + (math.cos(kszog)))
+    df1 = (float(d1) - (2*float(m)) - (2*float(c)))
+    df2 = (float(d2) - (2*float(m)) - (2*float(c)))
+    h1 = (float(m)*2)+float(c)
+    h2 = (float(m)*2)+float(c)
+    ha1 = float(m)
+    ha2 = float(m)
+    hf1 = (float(m)+float(c))
+    hf2 = (float(m)+float(c))
+    p = (float(m)*3.14)
+    s1 = ((float(p)/2))*(math.tan(kszog))
+    s2 = ((float(p)/2))*(math.tan(kszog))
+    hw = (float(m)*2)
+    aw = (float(d1)+float(d2))/2
+
+    datasE = []
+    datasE.append("\nAlap adatok:\nz1: " + str(z1) + "\nz2: " + str(z2) + "\nm: " + str(m) + "mm\nc*: " + str(cstar) + "\n-------------")
+    datasE.append("\nd1: "+ str(float(str(kerekites).format(d1)))+"mm")
+    datasE.append("d2: "+ str(float(str(kerekites).format(d2)))+"mm")
+    datasE.append("\nda1: "+ str(float(str(kerekites).format(da1)))+"mm")
+    datasE.append("da2: "+ str(float(str(kerekites).format(da2)))+"mm")
+    datasE.append("\ndb1: "+ str(float(str(kerekites).format(db1)))+"mm")
+    datasE.append("db2: "+ str(float(str(kerekites).format(db2)))+"mm")
+    datasE.append("\ndf1: "+ str(float(str(kerekites).format(df1)))+"mm")
+    datasE.append("df2: "+ str(float(str(kerekites).format(df2)))+"mm")
+    datasE.append("\nh1: "+ str(float(str(kerekites).format(h1)))+"mm")
+    datasE.append("h2: "+ str(float(str(kerekites).format(h2)))+"mm")
+    datasE.append("\nha1: "+ str(float(str(kerekites).format(ha1)))+"mm")
+    datasE.append("ha2: "+ str(float(str(kerekites).format(ha2)))+"mm")
+    datasE.append("\nhf1: "+ str(float(str(kerekites).format(hf1)))+"mm")
+    datasE.append("hf2: "+ str(float(str(kerekites).format(hf2)))+"mm")
+    datasE.append("\nhw: "+ str(float(str(kerekites).format(hw)))+"mm")
+    datasE.append("aw: "+ str(float(str(kerekites).format(aw)))+"mm")
+    datasE.append("\nP: "+ str(float(str(kerekites).format(p)))+"mm")
+    datasE.append("s1: "+ str(float(str(kerekites).format(s1)))+"mm")
+    datasE.append("s2: "+ str(float(str(kerekites).format(s2)))+"mm")
+
+    for data in datasE:
+        print(data)
+
+    time.sleep(0.5)
+    datasdone = True
+    print(bcolors.GREEN + '\nSzámítás kész!'+ bcolors.WHITE)
+    print(bcolors.YELLOW + "\nNavigálási lehetőségek:\n"+bcolors.BG2 +"Főmenü - 1"+ bcolors.WHITE)
+    print(bcolors.BG2 +"Kilépés - 2"+ bcolors.WHITE)
+    navInput = input(bcolors.BLUE + "\nNavigáció: " + bcolors.WHITE)
+    #navigate()
+
+if menuActive:
+    
+    if int(typeInput) > 0 and int(typeInput) <= 2:
         
-        
-
-# kompenzált
-elif kompActive:
-    if int(inputZ1) < 17:
-        z1 = inputZ1
-        inputZ2 = input("z2: ")
-    elif int(inputZ1) > 16:
-        print(bcolors.RED + "Hiba z1 nagyobb mint 16" + bcolors.WHITE)
-        sys.exit()
+        if int(typeInput) == 1:
+            clear()
+            print(bcolors.YELLOW + "\nElemi fogaskerék számítás" + bcolors.WHITE)
+            #defaultActive = True
+            inputZ1 = input("Z1: ")
+            
+            if int(inputZ1) > 16:
+                z1 = inputZ1
+                inputZ2 = input("z2: ")
+            elif int(inputZ1) < 17:
+                print(bcolors.RED + "Hiba z1 kisebb mint 17" + bcolors.WHITE)
+                time.sleep(0.5)
+                clear()
+                os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
     
-    if int(inputZ2) > 17:
-        z2 = inputZ2
-        inputM = input("m: ")
-    elif int(inputZ2) < 16:
-        print(bcolors.RED + "Hiba z2 kisebb mint 16" + bcolors.WHITE)
-        sys.exit()
+            if int(inputZ2) > 16:
+                z2 = inputZ2
+                inputM = input("m: ")
+            elif int(inputZ2) < 17:
+                print(bcolors.RED + "Hiba z2 kisebb mint 17" + bcolors.WHITE)
+                time.sleep(0.5)
+                clear()
+                os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
     
-    if int(float(inputM)) > 0:
-        m = float(inputM)
-        inputCstar = input("c*: ")
-    elif int(inputM) < 1:
-        print(bcolors.RED + "Hiba: m nem lehet 0" + bcolors.WHITE)
+            if int(float(inputM)) > 0:
+               m = float(inputM)
+               inputCstar = input("c*: ")
+            elif int(inputM) < 1:
+                print(bcolors.RED + "Hiba: m nem lehet 0" + bcolors.WHITE)
+                time.sleep(0.5)
+                clear()
+                os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+            if int(float(inputCstar)) >= 0:
+                cstar = float(inputCstar)
+                kerekitesInput = input("Kerekítés(2,3,4,5,6): ")
+
+            if int(kerekitesInput) >= 2 and int(kerekitesInput) <= 6:
+                kerekites = "{:."+kerekitesInput+"f}"
+                loadElemi()
+        elif int(typeInput) == 2:
+            #clear()
+            #print(bcolors.YELLOW + "\nKompenzált fogaskerék számítás" + bcolors.WHITE)
+            #kompActive = True
+            #inputZ1 = input("Z1: ")
+            #clear()
+            time.sleep(0.5)
+            os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+    else:
+        clear()
+        time.sleep(0.5)
+        os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+
+if int(navInput) > 0 and int(navInput) <= 2:
+    if int(navInput) == 1:
+        time.sleep(0.5)
+        os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+    else:
         sys.exit()
+else:
+    sys.exit()
 
-    if int(float(inputCstar)) >= 0:
-        cstar = float(inputCstar)
-        kerekitesInput = input("Kerekítés(2,3,4,5,6): ")
-
-    if int(kerekitesInput) >= 2 and int(kerekitesInput) <= 6:
-        kerekites = "{:."+kerekitesInput+"f}"
-        datasdone = True
-
-        if datasdone:
-            zhat = 17
-            kszog = math.radians(20)
-            c = float(m) * float(cstar)
-            x = (float(zhat) - float(z1)) / float(zhat)
-            x12 = (float(x)*float(m))
-            d1 = float(m) * float(z1)
-            d2 = float(m) * float(z2)
-            da1 = (float(d1) + (2*float(m))) + (2*float(str(kerekites).format(x12)))
-            da2 = (float(d2) + (2*float(m))) - (2*float(str(kerekites).format(x12)))
-            db1 = (float(d1) + (math.cos(kszog))) + (2*float(str(kerekites).format(x12)))
-            db2 = (float(d2) + (math.cos(kszog))) - (2*float(str(kerekites).format(x12)))
-            df1 = (float(d1) - (2*float(m)) - (2*float(c))) + (2*float(str(kerekites).format(x12)))
-            df2 = (float(d2) - (2*float(m)) - (2*float(c))) - (2*float(str(kerekites).format(x12)))
-            h1 = (float(m)*2)+float(c)
-            h2 = (float(m)*2)+float(c)
-            ha1 = float(m) + float(str(kerekites).format(x12))
-            ha2 = float(m) - float(str(kerekites).format(x12))
-            hf1 = (float(m)+float(c)) - float(str(kerekites).format(x12))
-            hf2 = (float(m)+float(c)) + float(str(kerekites).format(x12))
-            p = (float(m)*3.14)
-            s1veg = 2*float(str(kerekites).format(x12))*(math.tan(kszog))
-            s1 = ((float(p)/2))+s1veg
-            s2 = ((float(p)/2))-((2*float(str(kerekites).format(x12)))*(math.tan(kszog)))
-            hw = (float(m)*2)
-            aw = (float(d1)+float(d2))/2
-
-            datas = []
-            datas.append("\nAlap adatok:\nz1: " + str(z1) + "\nz2: " + str(z2) + "\nm: " + str(m) + "mm\nc*: " + str(cstar) + "\n-------------")
-            datas.append("\nx1,2: "+ str(float(str(kerekites).format(x12)))+"mm")
-            datas.append("\nd1: "+ str(float(str(kerekites).format(d1)))+"mm")
-            datas.append("d2: "+ str(float(str(kerekites).format(d2)))+"mm")
-            datas.append("\nda1: "+ str(float(str(kerekites).format(da1)))+"mm")
-            datas.append("da2: "+ str(float(str(kerekites).format(da2)))+"mm")
-            datas.append("\ndb1: "+ str(float(str(kerekites).format(db1)))+"mm")
-            datas.append("db2: "+ str(float(str(kerekites).format(db2)))+"mm")
-            datas.append("\ndf1: "+ str(float(str(kerekites).format(df1)))+"mm")
-            datas.append("df2: "+ str(float(str(kerekites).format(df2)))+"mm")
-            datas.append("\nh1: "+ str(float(str(kerekites).format(h1)))+"mm")
-            datas.append("h2: "+ str(float(str(kerekites).format(h2)))+"mm")
-            datas.append("\nha1: "+ str(float(str(kerekites).format(ha1)))+"mm")
-            datas.append("ha2: "+ str(float(str(kerekites).format(ha2)))+"mm")
-            datas.append("\nhf1: "+ str(float(str(kerekites).format(hf1)))+"mm")
-            datas.append("hf2: "+ str(float(str(kerekites).format(hf2)))+"mm")
-            datas.append("\nhw: "+ str(float(str(kerekites).format(hw)))+"mm")
-            datas.append("aw: "+ str(float(str(kerekites).format(aw)))+"mm")
-            datas.append("\nP: "+ str(float(str(kerekites).format(p)))+"mm")
-            datas.append("s1: "+ str(float(str(kerekites).format(s1)))+"mm")
-            datas.append("s2: "+ str(float(str(kerekites).format(s2)))+"mm")
-
-            for data in datas:
-                print(data)
